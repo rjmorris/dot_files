fb7e237ee3b413bdb23cd3c9963d5cf5bb138ba0
Joey Morris <rjmorris12@gmail.com>
2018-01-12 07:43:34 -0500

    Add Xmodmap

--- install.sh
+++ install.sh
@@ -3,6 +3,7 @@ cd $HOME
 ln -sf $HOME/.dot_files/bash/bashrc .bashrc
 ln -sf $HOME/.dot_files/bash/inputrc .inputrc
 ln -sf $HOME/.dot_files/bash/bash_profile .bash_profile
+ln -sf $HOME/.dot_files/Xmodmap .Xmodmap
 
 mkdir -p .config/git
 ln -sf $HOME/.dot_files/git/ignore .config/git/ignore
