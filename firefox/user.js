7313ecd321628dc4ac0d0c84e0fa777b581d678d
Joey Morris <rjmorris@rti.org>
2019-04-23 12:49:03 -0400

    Change the Firefox homepage to a blank page

--- firefox/user.js
+++ firefox/user.js
@@ -22,7 +22,7 @@ user_pref("browser.preferences.defaultPerformanceSettings.enabled", false);
 user_pref("browser.search.context.loadInBackground", true);
 user_pref("browser.search.hiddenOneOffs", "Startpage,Google,Yahoo,Bing,Amazon.com,Debian packages,DuckDuckGo,eBay,Twitter,Wikipedia (en)");
 user_pref("browser.search.suggest.enabled", false);
-user_pref("browser.startup.homepage", "/home/rjmorris/private_html/start.html");
+user_pref("browser.startup.homepage", "about:blank");
 user_pref("browser.startup.page", 3);
 user_pref("browser.tabs.closeWindowWithLastTab", false);
 user_pref("browser.tabs.warnOnClose", false);
