06bd18e6819e364367372e91d2021409c82db1a5
Joey Morris <rjmorris12@gmail.com>
2019-09-08 16:57:52 -0400

    Add ~/.Xresources

--- install.sh
+++ install.sh
@@ -3,7 +3,8 @@ cd $HOME
 ln -sf $HOME/.dot_files/bash/bashrc .bashrc
 ln -sf $HOME/.dot_files/bash/inputrc .inputrc
 ln -sf $HOME/.dot_files/bash/bash_profile .bash_profile
-ln -sf $HOME/.dot_files/Xmodmap .Xmodmap
+ln -sf $HOME/.dot_files/X/Xmodmap .Xmodmap
+ln -sf $HOME/.dot_files/X/Xresources .Xresources
 ln -sf $HOME/.dot_files/R/Rprofile .Rprofile
 
 mkdir -p .config/git
