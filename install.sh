fb4b883ee2581bb1e22fa5eda5974497dd5cd255
Joey Morris <rjmorris12@gmail.com>
2019-09-13 18:29:55 -0400

    Add GTK 3.0 settings file

--- install.sh
+++ install.sh
@@ -10,3 +10,6 @@ ln -sf $HOME/.dot_files/R/Rprofile .Rprofile
 mkdir -p .config/git
 ln -sf $HOME/.dot_files/git/config .config/git/config
 ln -sf $HOME/.dot_files/git/ignore .config/git/ignore
+
+mkdir -p .config/gtk-3.0
+ln -sf $HOME/.dot_files/gtk/settings.ini .config/gtk/settings.ini
